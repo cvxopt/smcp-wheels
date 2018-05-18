0d923344bf22c269e00ecbf277d1cba9eed02923
Martin S. Andersen <martin.skovgaard.andersen@gmail.com>
2018-05-18 15:13:30 +0200

    Build v0.4.6

--- config.sh
+++ config.sh
@@ -7,6 +7,7 @@ TESTS_DIR="$(pwd)/smcp"
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
+    python -c 'import smcp'
     python -c 'from smcp import *'
     python ${TESTS_DIR}/example.py
 }
